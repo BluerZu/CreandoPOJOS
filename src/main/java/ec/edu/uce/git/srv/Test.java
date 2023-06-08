4293d7307af930c6911c4147f3953ab64759dfea
BluerZu <superluis222@hotmail.com>
2023-06-08 00:21:37 -0500

    Creando mi segundo POJO

--- src/main/java/ec/edu/uce/git/srv/Test.java
+++ src/main/java/ec/edu/uce/git/srv/Test.java
@@ -4,12 +4,20 @@ public class Test {
 
     public void saludo() {
         System.out.println("Hola, bienvenido a mi primer POJO con lombok.");
+        //Creando al primer POJO:
         Persona p1 = new Persona();
         p1.setCedula("2233445566");
         p1.setNombres("Reigen");
         p1.setApellidos("Arataka");
 
-        System.out.println(p1);
+        System.out.println("El primer POJO es: "+p1);
 
+        //Creando el segundo POJO pasándole los valores del primero:
+        Persona p2 = new Persona(p1.getCedula(),"Shigeo", "Kageyama","08-06-2023");
+        System.out.println("El segundo POJO es: "+p2);
+
+        //Modificando la cédula del segundo POJO:
+        p2.setCedula("1223344556");
+        System.out.println("El segundo POJO modificado es: "+p2);
     }
 }
